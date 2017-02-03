a26ca950c2910e60aef0b67b62d372ce7d979d80
liriose <liriose@hotmail.fr>
2017-02-03 18:33:46 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,15 @@
-$.getJSON( "ajax/test.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+var myObj = this.responseText;
+var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+document.getElementById("demo").innerHTML = jsonPretty;
+}
+};
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.send();
+à mettre dans le fichier index.html
+<pre>
+<code id="demo" class="json kljs"></code>
+</pre>
