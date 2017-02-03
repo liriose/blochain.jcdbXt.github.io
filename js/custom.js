875322f8182e69c7c9cb2d5a41a35574522b4270
liriose <liriose@hotmail.fr>
2017-02-03 19:14:37 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,33 @@ document.getElementById("demo").innerHTML = jsonPretty;
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
 xmlhttp.send();
-xmlhttp.open("GET", "http://bitcoin.mubiz.com/blockchaininfo", true);
-xmlhttp.send();
-xmlhttp.open("GET", "http://bitcoin.mubiz.com/peerinfo", true);
-xmlhttp.send();
-xmlhttp.open("GET", "http://bitcoin.mubiz.com/mininginfo", true);
-xmlhttp.send();
+var xmlhttp1 = new XMLHttpRequest();
+xmlhttp1.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+var myObj = this.responseText;
+var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+document.getElementById("demo").innerHTML = jsonPretty;
+}
+};
+xmlhttp1.open("GET", "http://bitcoin.mubiz.com/blockchaininfo", true);
+xmlhttp1.send();
+var xmlhttp2 = new XMLHttpRequest();
+xmlhttp2.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+var myObj = this.responseText;
+var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+document.getElementById("demo").innerHTML = jsonPretty;
+}
+};
+xmlhttp2.open("GET", "http://bitcoin.mubiz.com/peerinfo", true);
+xmlhttp2.send();
+var xmlhttp3 = new XMLHttpRequest();
+xmlhttp3.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+var myObj = this.responseText;
+var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+document.getElementById("demo").innerHTML = jsonPretty;
+}
+};
+xmlhttp3.open("GET", "http://bitcoin.mubiz.com/mininginfo", true);
+xmlhttp3.send();
